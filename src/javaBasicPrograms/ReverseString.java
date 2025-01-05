0b48ad8e34846bc5c3afb06e17643ad160ba11d8
shobhit tiwari <shobhit tiwari@LAPTOP-881P9MV9>
2025-01-05 16:18:45 +0530

    4>ReverseString

---
+++ src/javaBasicPrograms/ReverseString.java
@@ -0,0 +1,10 @@
+package javaBasicPrograms;
+
+public class ReverseString {
+	 public static void main(String[] args) {
+	        String str = "Hello";
+	        String reversed = new StringBuilder(str).reverse().toString();
+	        System.out.println("Reversed String: " + reversed);
+	    }
+
+}
